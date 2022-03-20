8bdd8828d877253e1c5445637ae5172fd5e8eaac
Emmanuel jesus Campos <100785629+ManuuuCampos@users.noreply.github.com>
2022-03-20 14:37:56 -0300

    Two CommitIIII

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -120,7 +120,7 @@ function fizzBuzz(numero) {
   else if ((numero /3 && numero /5) === 0) console.log("fizzbuzz")
   else console.log(numero)
  }
- 
+
 function operadoresLogicos(num1, num2, num3) {
   //La función recibe tres números distintos. 
   //Si num1 es mayor a num2 y a num3 y además es positivo, retornar ---> "Número 1 es mayor y positivo"
@@ -128,8 +128,15 @@ function operadoresLogicos(num1, num2, num3) {
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
-}
-
+  var num1= Number
+  var num2= Number
+  var num3= Number
+  if (num1>num2 && num1>num3) console.log("Numero 1 es mayor y positivo")
+ else if (num1 === 0, num2 === 0 ,num3 === 0) console.log ("Error")
+ else if ((num3>num1 && num3>num2)+1) console.log(num3)
+ else false
+  }
+  
 function esPrimo(numero) {
   // Devuelve "true" si "numero" es primo
   // De lo contrario devuelve "falso"
